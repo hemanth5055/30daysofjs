3da64931dd8c8ab1ac65cb008fce5542f2995901
Kanala Hemanth Reddy <kanalahemanthreddy@Kanalas-MacBook-Air.local>
2024-11-18 17:01:19 +0530

    Version-1

---
+++ script.js
@@ -0,0 +1,28 @@
+let data = [
+  //   {
+  //     name: "Digital Clock",
+  //     description:
+  //       "A digital clock that displays the current time with real-time updates.",
+  //     proxyname: "digitalclock",
+  //   },
+];
+let cards = document.querySelector(".cards");
+let html = ``;
+data.forEach((element) => {
+  html += `<div class="card">
+                    <div class="cardimage">
+                    <img src="assets/${element.proxyname}.png" alt="">
+                    </div>
+                    <div class="cardname">
+                        ${element.name}
+                    </div>
+                    <div class="carddesc">
+                    ${element.description}
+                    </div>
+                    <div class="cardbtn">
+                        <div class="live"><a href="projects/${element.proxyname}/index.html">Live</a></div>
+                        <div class="githubc"><a href="https://github.com/hemanthrdygit/30daysjs/tree/main/projects/${element.proxyname}">Github</a></div>
+                    </div>
+                </div>`;
+});
+cards.innerHTML = html;
