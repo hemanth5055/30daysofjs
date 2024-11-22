74ea82d91fc921e376f60cfe1030c00a4df32f13
hemanth5055 <kanalahemanthrdy@gmail.com>
2024-11-22 14:43:43 +0530

    Added local storage functionality to todo app

--- Projects/todo/script.js
+++ Projects/todo/script.js
@@ -1,43 +1,58 @@
-let tasks = [];
+let tasks;
 let taskinp = document.querySelector(".taskinp");
 let addbtn = document.querySelector(".addbtn");
 let tasksdisplayarea = document.querySelector(".tasksdisplayarea");
 let clear = document.querySelector(".clear");
-let uid = 0;
+let uid = localStorage.getItem("uid")
+  ? parseInt(localStorage.getItem("uid"))
+  : 0;
+
+if (localStorage.getItem("data")) {
+  tasks = JSON.parse(localStorage.getItem("data"));
+} else {
+  tasks = [];
+  localStorage.setItem("data", JSON.stringify(tasks));
+}
+render();
+
+function updatelocalstorage() {
+  localStorage.setItem("data", JSON.stringify(tasks));
+}
+
 addbtn.addEventListener("click", () => {
   let temp = taskinp.value;
-  if (temp != "") {
+  taskinp.value = "";
+  if (temp !== "") {
     let tempObj = { id: uid, t: temp };
     uid++;
     tasks.push(tempObj);
     render();
+    updatelocalstorage();
+    localStorage.setItem("uid", uid);
   } else {
     console.log("Enter the task");
   }
 });
+
 function render() {
   let html = ``;
   for (let i = tasks.length - 1; i >= 0; i--) {
     html += `<div class="task">
-            <h4>${tasks[i].t}</h4>
-            <div class="del" onclick="deleter(${tasks[i].id})">x</div>
-          </div>`;
+              <div class="taskname"><h2>${tasks[i].t}</h2></div>
+              <div class="del" onclick="deleter(${tasks[i].id})">x</div>
+            </div>`;
   }
   tasksdisplayarea.innerHTML = html;
 }
 
 function deleter(did) {
-  let modifiedtasks = [];
-  for (let i = 0; i < tasks.length; i++) {
-    if (did != String(tasks[i].id)) {
-      modifiedtasks.push(tasks[i]);
-    }
-  }
-  tasks = modifiedtasks;
+  tasks = tasks.filter((task) => task.id !== did);
   render();
+  updatelocalstorage();
 }
 
 clear.addEventListener("click", () => {
   tasks = [];
   render();
+  updatelocalstorage();
 });
