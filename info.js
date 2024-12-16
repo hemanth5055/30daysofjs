c2fb20641989902733eb6417349e330a015c1653
hemanthrdygit <hemanthreddy.kanala3@gmail.com>
2024-12-16 11:04:08 +0530

    Complete UI change

---
+++ info.js
@@ -0,0 +1,170 @@
+let data = [
+  {
+    id: 1,
+    name: "Digital Clock",
+    description:
+      "A digital clock that displays the current time with real-time updates.",
+    proxyname: "digitalclock",
+  },
+  {
+    id: 2,
+    name: "Modern Number Counter",
+    description: "Increment or decrement numbers with a counter control.",
+    proxyname: "moderncounter",
+  },
+  {
+    id: 3,
+    name: "Color Picker",
+    description: "Select colors using a color picker and view the color codes.",
+    proxyname: "colorpicker",
+  },
+  {
+    id: 4,
+    name: "Todo App",
+    description: "Simple todo app to track your day-to-day tasks",
+    proxyname: "todo",
+  },
+  {
+    id: 5,
+    name: "Random Joke ",
+    description: "Generate random jokes using the JokeAPI.",
+    proxyname: "randomjoke",
+  },
+  {
+    id: 6,
+    name: "Random Color Generator",
+    description: "Generate random colors with a single click using this tool.",
+    proxyname: "randomcolorgenerator",
+  },
+  {
+    id: 7,
+    name: "Music Player",
+    description: "A simple music player built using HTML, CSS, and JavaScript.",
+    proxyname: "musicplayer",
+  },
+  {
+    id: 8,
+    name: "Analog Clock",
+    description: "Displays the current time with a simple clock interface.",
+    proxyname: "analogclock",
+  },
+  {
+    id: 9,
+    name: "Timer",
+    description: "Set a countdown in minutes for any task or event",
+    proxyname: "timer",
+  },
+  {
+    id: 10,
+    name: "Stopwatch",
+    description:
+      "A simple stopwatch with start, stop, and reset functionality.",
+    proxyname: "stopwatch",
+  },
+  {
+    id: 10,
+    name: "Password Generator",
+    description: "Generate strong random passwords for secure use.",
+    proxyname: "passwordgenerator",
+  },
+  {
+    id: 11,
+    name: "Tic Tac Toe",
+    description: "Play the classic Tic Tac Toe game against the computer.",
+    proxyname: "tictactoe",
+  },
+  {
+    id: 12,
+    name: "Weather App",
+    description:
+      "Get the weather details of any city using the OpenWeatherMap API.",
+    proxyname: "weather",
+  },
+  {
+    id: 13,
+    name: "Gradient Generator",
+    description:
+      "Create beautiful CSS gradients easily with this gradient generator.",
+    proxyname: "gradientgenerator",
+  },
+  {
+    id: 14,
+    name: "Accordian",
+    description: "Answers to FAQ's using accordian model.",
+    proxyname: "accordian",
+  },
+  {
+    id: 15,
+    name: "Drag & Drop",
+    description: "Drag and drop elements from box to another",
+    proxyname: "draganddrop",
+  },
+  {
+    id: 16,
+    name: "Snap Scrolling",
+    description: "Snap scrolling demonstration.",
+    proxyname: "snapscroll",
+  },
+  {
+    id: 17,
+    name: "Mini Calendar",
+    description: "A mini calendar displaying date,day,month&year.",
+    proxyname: "minicalendar",
+  },
+  {
+    id: 18,
+    name: "Drum Kit",
+    description: "Play different drum sounds using this interactive drum kit.",
+    proxyname: "drumkit",
+  },
+  {
+    id: 19,
+    name: "Lorem Ipsum Generator",
+    description:
+      "Generate random Lorem Ipsum text by selecting number of sentence.",
+    proxyname: "loremgenerator",
+  },
+  {
+    id: 20,
+    name: "RPS Game",
+    description:
+      "Play the classic Rock, Paper, Scissors game against the computer.",
+    proxyname: "rps",
+  },
+  {
+    id: 21,
+    name: "Notes App",
+    description: "Take and manage notes easily with this app.",
+    proxyname: "notesapp",
+  },
+  {
+    id: 22,
+    name: "Emoji Switcher",
+    description: "Change emoji by hovering on it.",
+    proxyname: "emojichanger",
+  },
+  {
+    id: 23,
+    name: "Dictionary",
+    description: "Find meaning of any english word.",
+    proxyname: "dictionary",
+  },
+  {
+    id: 24,
+    name: "Recipe App",
+    description: "Learn the ingredients and method to prepare a recipe.",
+    proxyname: "recipeapp",
+  },
+  {
+    id: 25,
+    name: "Country Details",
+    description: "know the details of a country by it's name.",
+    proxyname: "countrydetails",
+  },
+  {
+    id: 26,
+    name: "Budget App",
+    description: "Track all your expenses with this app.",
+    proxyname: "budgetapp",
+  },
+];
--- script.js
+++ script.js
@@ -1,196 +1,46 @@
-let data = [
-  {
-    id: 1,
-    name: "Digital Clock",
-    description:
-      "A digital clock that displays the current time with real-time updates.",
-    proxyname: "digitalclock",
-  },
-  {
-    id: 2,
-    name: "Modern Number Counter",
-    description: "Increment or decrement numbers with a counter control.",
-    proxyname: "moderncounter",
-  },
-  {
-    id: 3,
-    name: "Color Picker",
-    description: "Select colors using a color picker and view the color codes.",
-    proxyname: "colorpicker",
-  },
-  {
-    id: 4,
-    name: "Todo App",
-    description: "Simple todo app to track your day-to-day tasks",
-    proxyname: "todo",
-  },
-  {
-    id: 5,
-    name: "Random Joke ",
-    description: "Generate random jokes using the JokeAPI.",
-    proxyname: "randomjoke",
-  },
-  {
-    id: 6,
-    name: "Random Color Generator",
-    description: "Generate random colors with a single click using this tool.",
-    proxyname: "randomcolorgenerator",
-  },
-  {
-    id: 7,
-    name: "Music Player",
-    description: "A simple music player built using HTML, CSS, and JavaScript.",
-    proxyname: "musicplayer",
-  },
-  {
-    id: 8,
-    name: "Analog Clock",
-    description: "Displays the current time with a simple clock interface.",
-    proxyname: "analogclock",
-  },
-  {
-    id: 9,
-    name: "Timer",
-    description: "Set a countdown in minutes for any task or event",
-    proxyname: "timer",
-  },
-  {
-    id: 10,
-    name: "Stopwatch",
-    description:
-      "A simple stopwatch with start, stop, and reset functionality.",
-    proxyname: "stopwatch",
-  },
-  {
-    id: 10,
-    name: "Password Generator",
-    description: "Generate strong random passwords for secure use.",
-    proxyname: "passwordgenerator",
-  },
-  {
-    id: 11,
-    name: "Tic Tac Toe",
-    description: "Play the classic Tic Tac Toe game against the computer.",
-    proxyname: "tictactoe",
-  },
-  {
-    id: 12,
-    name: "Weather App",
-    description:
-      "Get the weather details of any city using the OpenWeatherMap API.",
-    proxyname: "weather",
-  },
-  {
-    id: 13,
-    name: "Gradient Generator",
-    description:
-      "Create beautiful CSS gradients easily with this gradient generator.",
-    proxyname: "gradientgenerator",
-  },
-  {
-    id: 14,
-    name: "Accordian",
-    description: "Answers to FAQ's using accordian model.",
-    proxyname: "accordian",
-  },
-  {
-    id: 15,
-    name: "Drag & Drop",
-    description: "Drag and drop elements from box to another",
-    proxyname: "draganddrop",
-  },
-  {
-    id: 16,
-    name: "Snap Scrolling",
-    description: "Snap scrolling demonstration.",
-    proxyname: "snapscroll",
-  },
-  {
-    id: 17,
-    name: "Mini Calendar",
-    description: "A mini calendar displaying date,day,month&year.",
-    proxyname: "minicalendar",
-  },
-  {
-    id: 18,
-    name: "Drum Kit",
-    description: "Play different drum sounds using this interactive drum kit.",
-    proxyname: "drumkit",
-  },
-  {
-    id: 19,
-    name: "Lorem Ipsum Generator",
-    description:
-      "Generate random Lorem Ipsum text by selecting number of sentence.",
-    proxyname: "loremgenerator",
-  },
-  {
-    id: 20,
-    name: "RPS Game",
-    description:
-      "Play the classic Rock, Paper, Scissors game against the computer.",
-    proxyname: "rps",
-  },
-  {
-    id: 21,
-    name: "Notes App",
-    description:
-      "Take and manage notes easily with this app.",
-    proxyname: "notesapp",
-  },
-  {
-    id: 22,
-    name: "Emoji Switcher",
-    description:
-      "Change emoji by hovering on it.",
-    proxyname: "emojichanger",
-  },
-  {
-    id: 23,
-    name: "Dictionary",
-    description:
-      "Find meaning of any english word.",
-    proxyname: "dictionary",
-  },
-  {
-    id: 24,
-    name: "Recipe App",
-    description:
-      "Learn the ingredients and method to prepare a recipe.",
-    proxyname: "recipeapp",
-  },
-  {
-    id: 25,
-    name: "Country Details",
-    description:
-      "know the details of a country by it's name.",
-    proxyname: "countrydetails",
-  },
-  {
-    id: 26,
-    name: "Budget App",
-    description:
-      "Track all your expenses with this app.",
-    proxyname: "budgetapp",
-  },
-];
-let cards = document.querySelector(".cards");
+let items = document.querySelector(".items");
+let mode = document.querySelector(".mode");
+let jsh1 = document.querySelector(".js h1");
+let root = document.querySelector(":root");
+
 let html = ``;
-data.forEach((element) => {
-  html += `<div class="card">
-                    <div class="cardimage">
-                    <img src="Assets/${element.proxyname}.png" alt="">
-                    </div>
-                    <div class="cardname">
-                        ${element.name}
-                    </div>
-                    <div class="carddesc">
-                    ${element.description}
-                    </div>
-                    <div class="cardbtn">
-                        <a class="live" href="Projects/${element.proxyname}/index.html">Live</a>
-                        <a class="githubc" href="https://github.com/hemanth5055/30daysofjs/tree/main/Projects/${element.proxyname}">Github</a>
-                    </div>
-                </div>`;
+let isDark = false;
+mode.addEventListener("click", () => {
+  if (isDark) {
+    //turn to light mode
+    root.style.setProperty("--text-color", "black");
+    root.style.setProperty("--card-background", "#eeeeee");
+    root.style.setProperty("--body-background", "white");
+    root.style.setProperty("--button-background", "#fbfbfb");
+    root.style.setProperty("--button-background-main", "#e3e3e3bc");
+    isDark = false;
+    mode.innerHTML = `<i class="fa-solid fa-moon" style="color: #ffffff;"></i>`;
+  } else {
+    //turn to dark mode
+    root.style.setProperty("--text-color", "white");
+    root.style.setProperty("--card-background", "#2a2a2a");
+    root.style.setProperty("--body-background", "black");
+    root.style.setProperty("--button-background", "#545454");
+    root.style.setProperty("--button-background-main", "#545454");
+    isDark = true;
+    mode.innerHTML = `<i class="fa-solid fa-lightbulb" style="color: #ffffff;">`;
+  }
 });
-cards.innerHTML = html;
+data.forEach((element) => {
+  html += `<div class="item">
+          <div class="itemimg">
+          <img src="Assets/${element.proxyname}.png" alt="">
+          </div>
+          <div class="itemdetails">
+            <h3 class="headingitem">${element.name}</h3>
+            <p class="decription">
+             ${element.description}
+            </p>
+            <div class="btns1">
+              <a href="Projects/${element.proxyname}/index.html" target="_blank"><button class="livec bt1">Live</button></a>
+              <a href="https://github.com/hemanth5055/30daysofjs/tree/main/Projects/${element.proxyname}" target="_blank"><button class="githubc bt1">Github</button></a>
+            </div>
+          </div>
+        </div>`;
+});
+items.innerHTML = html;
